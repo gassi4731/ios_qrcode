2d74bf49f73494bb92ef1cfb5d581973d009e673
Yo Higashida <penguin4731@gmail.com>
2021-04-19 16:47:43 +0900

    :tada: QRコード生成

--- qrcode/ViewController.swift
+++ qrcode/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // 定義
+    @IBOutlet var urlTextField: UITextField!
+    @IBOutlet var qrImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func submitButton() {
+        qrImageView.image = QRCode
+    }
+    
+    // QRコード生成
+    private var QRCode: UIImage? {
+        // 何も入力されていない場合は、処理を中断する
+        guard let url = urlTextField.text else {
+            return nil
+        }
+        // URLをデータ型に変換
+        let data = url.data(using: .utf8)!
+        
+        // QRコードをCIFilterで生成
+        // エラー修正レベル（inputCorrectionLevel）は、L→M→Q→Hの順で高くなり細かくなる。
+        let qr = CIFilter(name: "CIQRCodeGenerator", parameters: ["inputMessage": data, "inputCorrectionLevel": "L"])!
+        let image = UIImage(ciImage: qr.outputImage!)
+        return image
+    }
 }
-
